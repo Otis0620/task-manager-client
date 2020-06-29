02b61763f11f4a9f96841c9411d3ff7abcde90fe
Otis0620 <sharding1012@gmail.com>
2020-06-28 23:02:56 -0700

    refactor actions to use async/await

--- src/actions/swim-lanes.ts
+++ src/actions/swim-lanes.ts
@@ -3,25 +3,24 @@ import { hasErrored, isLoading } from './shared';
 import { GET_SWIMLANES_DATA_SUCCESS } from '../action-types';
 
 export const getSwimLanesData = (url: string): Function => {
-  return (dispatch: Function) => {
+  return async (dispatch: Function) => {
     dispatch(isLoading(true));
 
-    axios
-      .get(url)
-      .then(({ data, status, statusText }) => {
-        if (!(status === 200)) {
-          throw Error(statusText);
-        }
+    try {
+      const { data, status, statusText } = await axios.get(url);
 
-        dispatch(isLoading(false));
+      if (!(status === 200)) {
+        throw Error(statusText);
+      }
 
-        dispatch({
-          type: GET_SWIMLANES_DATA_SUCCESS,
-          swimLanes: data,
-        });
-      })
-      .catch(() => {
-        dispatch(hasErrored(true));
+      dispatch(isLoading(false));
+
+      dispatch({
+        type: GET_SWIMLANES_DATA_SUCCESS,
+        swimLanes: data,
       });
+    } catch {
+      dispatch(hasErrored(true));
+    }
   };
 };
--- src/actions/tasks.ts
+++ src/actions/tasks.ts
@@ -3,21 +3,20 @@ import { GET_TASKS_DATA_SUCCESS } from '../action-types';
 import { hasErrored } from './shared';
 
 export const getTaskData = (url: string): Function => {
-  return (dispatch: Function) => {
-    axios
-      .get(url)
-      .then(({ data, status, statusText }) => {
-        if (!(status === 200)) {
-          throw Error(statusText);
-        }
+  return async (dispatch: Function) => {
+    try {
+      const { data, status, statusText } = await axios.get(url);
 
-        dispatch({
-          type: GET_TASKS_DATA_SUCCESS,
-          tasks: data,
-        });
-      })
-      .catch(() => {
-        dispatch(hasErrored(true));
+      if (!(status === 200)) {
+        throw Error(statusText);
+      }
+
+      dispatch({
+        type: GET_TASKS_DATA_SUCCESS,
+        tasks: data,
       });
+    } catch {
+      dispatch(hasErrored(true));
+    }
   };
 };
