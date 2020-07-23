e6008fc955a3ef869a32457b2ba5cbdaa9fcf243
Otis0620 <sharding1012@gmail.com>
2020-07-23 11:55:09 -0700

    add tests for shared reducers

---
+++ src/reducers/shared.test.js
@@ -0,0 +1,28 @@
+import { hasErrored, isLoading } from './shared';
+import { HAS_ERRORED, IS_LOADING } from '../action-types';
+
+describe('shared', () => {
+  describe('hasErrored', () => {
+    it('should return the payload if type is HAS_ERRORED', () => {
+      const payload = [];
+
+      expect(hasErrored(true, { type: HAS_ERRORED, payload })).toEqual(payload);
+    });
+
+    it('should return state (true) if type is not HAS_ERRORED', () => {
+      expect(hasErrored(true, { type: 'NOT_HAS_ERRORED', payload: [] })).toBeTruthy();
+    });
+  });
+
+  describe('isLoading', () => {
+    it('should return the payload if type is IS_LOADING', () => {
+      const payload = [];
+
+      expect(isLoading(true, { type: IS_LOADING, payload })).toEqual(payload);
+    });
+
+    it('should return state (true) if type is not IS_LOADING', () => {
+      expect(isLoading(true, { type: 'NOT_IS_LOADING', payload: [] })).toBeTruthy();
+    });
+  });
+});
