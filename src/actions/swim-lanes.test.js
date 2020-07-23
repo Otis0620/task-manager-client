8a62a793fbec955e5678a3eee9a609b795a0bee6
Otis0620 <sharding1012@gmail.com>
2020-07-22 22:33:42 -0700

    add swim-lanes actions tests

---
+++ src/actions/swim-lanes.test.js
@@ -0,0 +1,46 @@
+import configureStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+import { getSwimLanesData } from './swim-lanes';
+import { GET_SWIMLANES_DATA_SUCCESS, HAS_ERRORED, IS_LOADING } from '../action-types';
+import { requestService } from '../services';
+
+jest.mock('../services');
+
+const middlewares = [thunk];
+const mockStore = configureStore(middlewares);
+
+describe('swimlanes', () => {
+  describe('getSwimLanesData', () => {
+    it('should dispatch IS_LOADING and GET_SWIMLANES_DATA_SUCCESS', async () => {
+      const store = mockStore({});
+      const payload = [];
+      const expectedActions = [
+        { type: IS_LOADING, payload: true },
+        { type: IS_LOADING, payload: false },
+        { type: GET_SWIMLANES_DATA_SUCCESS, payload },
+      ];
+
+      requestService.get.mockReturnValue(payload);
+
+      await store.dispatch(getSwimLanesData());
+
+      expect(store.getActions()).toEqual(expectedActions);
+    });
+  });
+
+  describe('getSwimLanesData', () => {
+    it('should dispatch IS_LOADING and HAS_ERRORED if api request fails', async () => {
+      const store = mockStore({});
+      const expectedActions = [
+        { type: IS_LOADING, payload: true },
+        { type: HAS_ERRORED, payload: true },
+      ];
+
+      requestService.get.mockRejectedValue();
+
+      await store.dispatch(getSwimLanesData());
+
+      expect(store.getActions()).toEqual(expectedActions);
+    });
+  });
+});
